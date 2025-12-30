e2263edea876f8d57f52056f4433e8b584e66741
owl <oyikavamiko@gmail.com>
2025-12-30 19:09:35 +0300

    redis to lazy initialization

--- src/Infrastructure/Redis/Redis.cs
+++ src/Infrastructure/Redis/Redis.cs
@@ -4,28 +4,38 @@ namespace Redis;
 
 public static class Redis
 {
-    private static readonly Dictionary<string, IConnectionMultiplexer> Multiplexers = [];
+    private static readonly Dictionary<string, Lazy<IConnectionMultiplexer>> Multiplexers = [];
     public static bool IsConfigured { get; private set; }
 
     public static void Configure(string configurationString)
     {
         if (IsConfigured) throw new InvalidOperationException("Cannot set pool manager twice");
         IsConfigured = true;
-        Multiplexers.Add("default", ConnectionMultiplexer.Connect(configurationString));
+        Multiplexers.Add("default", new Lazy<IConnectionMultiplexer>(() =>
+        {
+            var options = ConfigurationOptions.Parse(configurationString);
+            options.AbortOnConnectFail = false;
+            return ConnectionMultiplexer.Connect(options);
+        }));
     }
 
     public static void Configure(Dictionary<string, string> configurationStrings)
     {
         if (IsConfigured) throw new InvalidOperationException("Cannot set pool manager twice");
         IsConfigured = true;
-        foreach (var configurationString in configurationStrings)
-            Multiplexers.Add(configurationString.Key, ConnectionMultiplexer.Connect(configurationString.Value));
+        foreach (var (name,  configurationString) in configurationStrings)
+            Multiplexers.Add(name, new Lazy<IConnectionMultiplexer>(() =>
+            {
+                var options = ConfigurationOptions.Parse(configurationString);
+                options.AbortOnConnectFail = false;
+                return ConnectionMultiplexer.Connect(options);
+            }));
     }
 
     public static IDatabase GetRedis(string name = "default")
     {
         if (!IsConfigured) throw new InvalidOperationException("Redis is not configured");
-        return Multiplexers[name].GetDatabase();
+        return Multiplexers[name].Value.GetDatabase();
     }
 
     public static async Task CloseAllConnections(CancellationToken token = default)
@@ -33,7 +43,7 @@ public static class Redis
         var keysToRemove = new List<string>();
         foreach (var (key, connection) in Multiplexers)
         {
-            await connection.CloseAsync();
+            await connection.Value.CloseAsync();
             keysToRemove.Add(key);
             if (token.IsCancellationRequested) break;
         }
