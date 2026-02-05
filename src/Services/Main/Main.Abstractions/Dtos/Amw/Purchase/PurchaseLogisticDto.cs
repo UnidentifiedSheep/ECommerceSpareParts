26cfff00960bb62009aade3ce1d242c2fc8c14e7
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-02-05 17:21:43 +0300

    purchase logistic dto updated.

--- src/Services/Main/Main.Abstractions/Dtos/Amw/Purchase/PurchaseLogisticDto.cs
+++ src/Services/Main/Main.Abstractions/Dtos/Amw/Purchase/PurchaseLogisticDto.cs
@@ -1,4 +1,5 @@
-﻿using Main.Enums;
+﻿using Main.Abstractions.Dtos.Currencies;
+using Main.Enums;
 
 namespace Main.Abstractions.Dtos.Amw.Purchase;
 
@@ -6,7 +7,6 @@ public class PurchaseLogisticDto
 {
     public string PurchaseId { get; set; } = null!;
     public Guid RouteId { get; set; }
-    public int CurrencyId { get; set; }
     public Guid? TransactionId { get; set; }
     public LogisticPricingType PricingModel { get; set; }
     public RouteType RouteType { get; set; }
@@ -15,6 +15,5 @@ public class PurchaseLogisticDto
     public decimal PricePerOrder { get; set; }
     public decimal? MinimumPrice { get; set; }
     public bool MinimumPriceApplied { get; set; }
-    public string CurrencySign { get; set; } = string.Empty;
-    public string CurrencyName { get; set; } = string.Empty;
+    public CurrencyDto Currency { get; set; } = null!;
 }
