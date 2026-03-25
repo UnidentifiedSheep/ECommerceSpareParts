1e76ecf05dfae93ad3e813e17856b466445c8965
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-03-25 17:30:00 +0300

    created WithLocalizationKey for fluent validation rule builder

---
+++ src/Infrastructure/Localization/Localization.Domain/Extensions/RuleBuilderOptionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Localization.Domain.Extensions;
+
+public static class RuleBuilderOptionExtensions
+{
+    public static IRuleBuilderOptions<T, TProperty> WithLocalizationKey<T, TProperty>(
+        this IRuleBuilderOptions<T, TProperty> rule,
+        string localizationKey)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(localizationKey);
+        DefaultValidatorOptions.Configurable(rule).Current.ErrorCode = localizationKey;
+        return rule;
+    }
+}
