fc1518a404ad60f4f6b1e24403be1673f5583eb7
Diyar Tashliyan <d.tashliyan@mindbox-external.cloud>
2026-05-03 16:40:31 +0300

    purchase tests created

---
+++ src/Services/Main/Main.Tests.Domain/Purchase/PurchaseContentLogisticTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using Main.Entities.Purchase;
+
+namespace Main.Tests.Domain.Purchase;
+
+public class PurchaseContentLogisticTests
+{
+    [Fact]
+    public void Create_ValidData_Succeeds()
+    {
+        var log = PurchaseContentLogistic.Create(1, 2, 3);
+
+        log.WeightKg.Should().Be(1);
+        log.AreaM3.Should().Be(2);
+        log.Price.Should().Be(3);
+    }
+
+    [Fact]
+    public void Create_NegativeWeight_Throws()
+    {
+        var act = () => PurchaseContentLogistic.Create(-1, 2, 3);
+
+        act.Should().Throw<InvalidOperationException>();
+    }
+    
+    [Fact]
+    public void Update_PartialConsistency_AllFieldsUpdatedAtomically()
+    {
+        var log = PurchaseContentLogistic.Create(1, 2, 3);
+
+        log.Update(10, 20, 30);
+
+        log.WeightKg.Should().Be(10);
+        log.AreaM3.Should().Be(20);
+        log.Price.Should().Be(30);
+    }
+    
+    [Fact]
+    public void Update_MultipleCalls_KeepsConsistentState()
+    {
+        var log = PurchaseContentLogistic.Create(1, 2, 3);
+
+        log.Update(5, 6, 7);
+        log.Update(8, 9, 10);
+        log.Update(11, 12, 13);
+
+        log.WeightKg.Should().Be(11);
+        log.AreaM3.Should().Be(12);
+        log.Price.Should().Be(13);
+    }
+
+    [Fact]
+    public void Create_HighPrecisionValues_ShouldBehaveConsistently()
+    {
+        var log = PurchaseContentLogistic.Create(1.123m, 2.456m, 3.789m);
+
+        log.WeightKg.Should().Be(1.123m);
+        log.AreaM3.Should().Be(2.456m);
+        log.Price.Should().Be(3.789m);
+    }
+    
+    [Fact]
+    public void Update_ChangesValues()
+    {
+        var log = PurchaseContentLogistic.Create(1, 2, 3);
+
+        log.Update(5, 6, 7);
+
+        log.WeightKg.Should().Be(5);
+        log.AreaM3.Should().Be(6);
+        log.Price.Should().Be(7);
+    }
+}
---
+++ src/Services/Main/Main.Tests.Domain/Purchase/PurchaseTests.cs
@@ -0,0 +1,62 @@
+﻿using Enums;
+using FluentAssertions;
+using Main.Entities.Purchase;
+using DomainPurchase = Main.Entities.Purchase.Purchase;
+
+namespace Main.Tests.Domain.Purchase;
+
+public class PurchaseTests
+{
+    [Fact]
+    public void Create_ValidData_Succeeds()
+    {
+        var purchase = DomainPurchase.Create(
+            Guid.NewGuid(),
+            1,
+            Guid.NewGuid(),
+            "WH-1",
+            DateTime.UtcNow);
+
+        purchase.State.Should().Be(PurchaseState.Draft);
+    }
+
+    [Fact]
+    public void Complete_ChangesState()
+    {
+        var purchase = Create();
+
+        purchase.Complete();
+
+        purchase.State.Should().Be(PurchaseState.Completed);
+    }
+
+    [Fact]
+    public void AddContent_WithWrongPurchaseId_Throws()
+    {
+        var purchase = Create();
+
+        var content = PurchaseContent.Create(
+            productId: 1,
+            count: 2,
+            price: 10m,
+            storageContentId: null);
+        
+        typeof(PurchaseContent)
+            .GetProperty("PurchaseId")!
+            .SetValue(content, Guid.NewGuid());
+
+        var act = () => purchase.AddContent(content);
+
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    private static DomainPurchase Create()
+    {
+        return DomainPurchase.Create(
+            Guid.NewGuid(),
+            1,
+            Guid.NewGuid(),
+            "WH-1",
+            DateTime.UtcNow);
+    }
+}
