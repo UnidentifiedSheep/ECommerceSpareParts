8088799ace1c1183a3157fe9e14b9fcdfc61270b
Diyar Tashliyan <d.tashliyan@mindbox-external.cloud>
2026-05-26 21:04:35 +0300

    fix analytics migration

---
+++ src/Services/Analytics/Analytics.Migrator/Migrations/20260526180317_AfterInit.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Analytics.Migrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class AfterInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "article_id",
+                table: "metrics",
+                newName: "product_id");
+
+            migrationBuilder.RenameIndex(
+                name: "analytics.entities.metrics.metric_who_updated_idx",
+                table: "metrics",
+                newName: "analytics.entities.metrics.productsalesmetric_who_updated_idx");
+
+            migrationBuilder.RenameIndex(
+                name: "analytics.entities.metrics.metric_who_created_idx",
+                table: "metrics",
+                newName: "analytics.entities.metrics.productsalesmetric_who_created_idx");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "product_id",
+                table: "metrics",
+                newName: "article_id");
+
+            migrationBuilder.RenameIndex(
+                name: "analytics.entities.metrics.productsalesmetric_who_updated_idx",
+                table: "metrics",
+                newName: "analytics.entities.metrics.metric_who_updated_idx");
+
+            migrationBuilder.RenameIndex(
+                name: "analytics.entities.metrics.productsalesmetric_who_created_idx",
+                table: "metrics",
+                newName: "analytics.entities.metrics.metric_who_created_idx");
+        }
+    }
+}
