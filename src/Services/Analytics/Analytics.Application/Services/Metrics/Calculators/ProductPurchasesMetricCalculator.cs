b0e14d2fc8f5800149f24132b040bf44a491cdbc
Diyar Tashliyan <d.tashliyan@mindbox-external.cloud>
2026-05-30 21:44:07 +0300

    fix calculator

--- src/Services/Analytics/Analytics.Application/Services/Metrics/Calculators/ProductPurchasesMetricCalculator.cs
+++ src/Services/Analytics/Analytics.Application/Services/Metrics/Calculators/ProductPurchasesMetricCalculator.cs
@@ -1,12 +1,93 @@
-﻿using Analytics.Entities.Metrics;
+﻿using Analytics.Entities;
+using Analytics.Entities.Metrics;
+using Analytics.Entities.Metrics.JsonDataModels;
+using Application.Common.Interfaces.Currency;
+using Application.Common.Interfaces.Repositories;
 
 namespace Analytics.Application.Services.Metrics.Calculators;
 
-public class ProductPurchasesMetricCalculator : MetricCalculatorBase<ProductPurchasesMetric>
+public class ProductPurchasesMetricCalculator(
+    IRepository<PurchasesFact, Guid> repository,
+    ICurrencyConverter currencyConverter) : MetricCalculatorBase<ProductPurchasesMetric>
 {
     public override async Task CalculateMetric(
         ProductPurchasesMetric metric,
         CancellationToken cancellationToken = default)
     {
+        var minPrice = decimal.MaxValue;
+        var maxPrice = decimal.MinValue;
+        decimal totalAmount = 0;
+        var totalQuantity = 0;
+
+        double mean = 0;
+        double m2 = 0;
+        long count = 0;
+
+
+        var (start, end) = await WithTimer(async () =>
+        {
+            await foreach (var fact in repository
+                               .AsyncEnumerable(GetCriteria(metric))
+                               .WithCancellation(cancellationToken))
+            foreach (var item in fact.PurchaseContents)
+            {
+                if (item.ArticleId != metric.ProductId)
+                    continue;
+
+                var priceDecimal = await currencyConverter.ConvertToBaseAsync(
+                    item.Price,
+                    fact.CurrencyId,
+                    cancellationToken);
+                var quantity = item.Count;
+
+                if (quantity <= 0)
+                    continue;
+
+                var price = (double)priceDecimal;
+
+                if (priceDecimal < minPrice) minPrice = priceDecimal;
+                if (priceDecimal > maxPrice) maxPrice = priceDecimal;
+
+                totalAmount += priceDecimal * quantity;
+                totalQuantity += quantity;
+
+                count += quantity;
+
+                var delta = price - mean;
+                mean += delta * quantity / count;
+                var delta2 = price - mean;
+                m2 += quantity * delta * delta2;
+            }
+        });
+
+        var variance = count > 1 ? m2 / count : 0.0;
+        var volatility = (decimal)Math.Sqrt(variance);
+
+        var avgPrice = totalQuantity == 0 ? 0 : totalAmount / totalQuantity;
+
+        var data = new ProductInfoModel
+        {
+            Quantity = totalQuantity,
+            TotalAmount = totalAmount,
+            PriceInfo = new PriceInfoModel
+            {
+                AveragePrice = avgPrice,
+                MaximumPrice = totalQuantity == 0 ? 0 : maxPrice,
+                MinimumPrice = totalQuantity == 0 ? 0 : minPrice,
+                Volatility = volatility
+            },
+            Timer = new MetricTimer(start, end)
+        };
+
+        metric.SetData(data);
+        metric.CompleteRecalculation();
+    }
+    
+    private static Criteria<PurchasesFact> GetCriteria(ProductPurchasesMetric metric)
+    {
+        return Criteria<PurchasesFact>.New()
+            .Where(x => x.PurchaseContents.Any(z => z.ArticleId == metric.ProductId)
+                        && metric.RangeStart <= x.CreatedAt && x.CreatedAt <= metric.RangeEnd)
+            .Build();
     }
 }
