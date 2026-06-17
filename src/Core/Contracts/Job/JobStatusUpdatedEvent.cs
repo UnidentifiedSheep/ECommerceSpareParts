2fec5a71ed72db5067ef56739a78849a77caf31d
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-06-17 09:51:59 +0300

    add property names for event

--- src/Core/Contracts/Job/JobStatusUpdatedEvent.cs
+++ src/Core/Contracts/Job/JobStatusUpdatedEvent.cs
@@ -1,11 +1,17 @@
+using System.Text.Json.Serialization;
 using Abstractions.Interfaces;
 
 namespace Contracts.Job;
 
 public record JobStatusUpdatedEvent : IKeyedEvent
 {
+    [JsonPropertyName("jobId")]
     public required Guid JobId { get; init; }
+    
+    [JsonPropertyName("status")]
     public required string Status { get; init; }
+    
+    [JsonPropertyName("attempts")]
     public required int CurrentAttempt { get; init; }
     public string GetKey() => $"job-status-updated:{JobId}";
 }
