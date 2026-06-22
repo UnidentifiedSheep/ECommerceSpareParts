44d9b4733c6fe7361b68d8771038574f5d4f49cf
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-06-22 10:54:10 +0300

    add user financial profile entity

---
+++ src/Services/Main/Main.Entities/Balance/UserFinancialProfile.cs
@@ -0,0 +1,40 @@
+using Domain;
+using Domain.Extensions;
+using Domain.Interfaces;
+
+namespace Main.Entities.Balance;
+
+public class UserFinancialProfile : AuditableEntity<UserFinancialProfile, Guid>, IVersionable<uint>
+{
+    public Guid UserId { get; private set; }
+    public decimal TotalBalance { get; private set; }
+    public decimal MinAllowedBalance { get; private set; }
+    public uint RowVersion { get; private set; }
+
+    public override Guid GetId() => UserId;
+
+    private UserFinancialProfile(Guid userId, decimal minAllowedBalance)
+    {
+        UserId = userId;
+        TotalBalance = 0;
+        MinAllowedBalance = minAllowedBalance;
+    }
+
+    public static UserFinancialProfile Create(Guid userId, decimal minAllowedBalance = 0)
+        => new(userId, minAllowedBalance);
+
+    public void Withdraw(decimal amount)
+    {
+        amount.AgainstNegative("financial.profile.amount.must.not.be.negative");
+        var newBalance = TotalBalance - amount;
+        newBalance.AgainstTooSmall(MinAllowedBalance, "financial.profile.balance.must.not.be.less.than.minimum");
+        TotalBalance = newBalance;
+    }
+    
+    public void Deposit(decimal amount)
+    {
+        amount.AgainstNegative("financial.profile.amount.must.not.be.negative");
+        var newBalance = TotalBalance + amount;
+        TotalBalance = newBalance;
+    }
+}
