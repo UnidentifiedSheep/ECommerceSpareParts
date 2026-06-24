70b91a36189c3cda9442b4ae8f812ab973545e19
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-06-24 10:50:54 +0300

    add GetCurrencies method to ICurrencyNode and implement in CurrencyNode

--- src/Infrastructure/Integrations/Internal/Internal.Integration.Core/Interfaces/Main/ICurrencyNode.cs
+++ src/Infrastructure/Integrations/Internal/Internal.Integration.Core/Interfaces/Main/ICurrencyNode.cs
@@ -1,6 +1,9 @@
+using Internal.Integration.Core.Models.Main;
+
 namespace Internal.Integration.Core.Interfaces.Main;
 
 public interface ICurrencyNode
 {
     Task<decimal> GetCurrencyRate(int currencyId, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<InternalCurrency>> GetCurrencies(CancellationToken cancellationToken = default);
 }
--- src/Infrastructure/Integrations/Internal/Internal.Integration.Main/CurrencyNode.cs
+++ src/Infrastructure/Integrations/Internal/Internal.Integration.Main/CurrencyNode.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 using Internal.Integration.Core;
 using Internal.Integration.Core.Interfaces;
 using Internal.Integration.Core.Interfaces.Main;
+using Internal.Integration.Core.Models.Main;
 using Microsoft.Extensions.Options;
 
 namespace Internal.Integration.Main;
@@ -31,10 +32,34 @@ internal sealed class CurrencyNode(
         return JsonSerializer.Deserialize<GetCurrencyRateResponse>(json)?.Rate
                ?? throw new InvalidOperationException($"{nameof(GetCurrencyRate)} returned null.");
     }
+
+    public async Task<IReadOnlyList<InternalCurrency>> GetCurrencies(
+        CancellationToken cancellationToken = default)
+    {
+        using var request = await GetRequest(
+            HttpMethod.Get,
+            "/currencies",
+            cancellationToken);
+        using var response = await httpClient.SendAsync(
+            request,
+            cancellationToken);
+
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadAsStringAsync(cancellationToken);
+        return JsonSerializer.Deserialize<GetCurrenciesResponse>(json)?.Currencies
+               ?? throw new InvalidOperationException($"{nameof(GetCurrencyRate)} returned null.");
+    }
     
     private record GetCurrencyRateResponse
     {
         [JsonPropertyName("rate")]
         public decimal Rate { get; init; }
+    }
+
+    private record GetCurrenciesResponse
+    {
+        [JsonPropertyName("currencies")]
+        public required IReadOnlyList<InternalCurrency> Currencies { get; init; }
     }
 }
