d4c3c058af75495962ee219134232f196e6bf185
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-07-27 20:03:07 +0300

    add filtering producers by IDs and integration tests

---
+++ src/Services/Main/Main.Tests/HandlersTests/Producers/GetProducerByIdTests.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+using Main.Application.Handlers.Producers;
+using Main.Entities.Exceptions;
+using Tests.DataBuilders;
+using Tests.Extensions;
+using Tests.TestContainers.Combined;
+
+namespace Tests.HandlersTests.Producers;
+
+public class GetProducerByIdTests(CombinedContainerFixture fixture) : IntegrationTest(fixture)
+{
+    [Fact]
+    public async Task GetProducerById_ExistingProducer_ReturnsProducer()
+    {
+        var producer = await new ProducerBuilder(Faker)
+            .WithName("Requested producer")
+            .WithDescription("Requested description")
+            .BuildAndAddToDb(Context);
+
+        var result = await Mediator.Send(new GetProducerByIdQuery(producer.Id));
+
+        result.Producer.Id.Should().Be(producer.Id);
+        result.Producer.Name.Should().Be(producer.Name);
+        result.Producer.Description.Should().Be(producer.Description);
+    }
+
+    [Fact]
+    public async Task GetProducerById_MissingProducer_ThrowsProducerNotFoundException()
+    {
+        var act = () => Mediator.Send(new GetProducerByIdQuery(int.MaxValue));
+
+        await act.Should().ThrowAsync<ProducerNotFoundException>();
+    }
+}
