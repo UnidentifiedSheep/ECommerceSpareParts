03fa9ed33a1bc0a0ab5a7246bfb80ffc399de86e
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-08-01 23:17:31 +0300

    seed created.

---
+++ src/Services/Main/Main.Migrator/DataSeeds/BaseScheduledJobsSeed.cs
@@ -0,0 +1,13 @@
+using Main.Persistence.Context;
+using Persistence.Interfaces;
+
+namespace Main.Migrator.DataSeeds;
+
+public class BaseScheduledJobsSeed : ISeed<DContext>
+{
+    public Task SeedAsync(DContext context)
+    {
+        return Task.CompletedTask; //TODO: we need to seed basic lrts like balances recalculation.
+    }
+    public int GetPriority() => int.MaxValue;
+}
