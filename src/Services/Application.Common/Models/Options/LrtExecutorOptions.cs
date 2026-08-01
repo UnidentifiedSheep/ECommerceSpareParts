dc730413881bad2115b7ab40f59167bdf077a9e2
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-08-01 20:01:22 +0300

    fix ExpiredJobsWiperHostedService

--- src/Services/Application.Common/Models/Options/LrtExecutorOptions.cs
+++ src/Services/Application.Common/Models/Options/LrtExecutorOptions.cs
@@ -1,9 +1,16 @@
-﻿namespace Application.Common.Models.Options;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Common.Models.Options;
 
 public class LrtExecutorOptions
 {
     public const string SectionName = "LrtExecutor";
+
+    [Range(1, int.MaxValue)]
     public int MaxParallelPerWorker { get; set; } = 3;
+
+    [Range(1, int.MaxValue)]
     public int MaxExpiredLeaseFailBatchSize { get; set; } = 100;
+
     public TimeSpan Delay { get; set; } = TimeSpan.FromSeconds(30);
 }
