4702703b86aa3fb3f154b58ceca93bdb80475c12
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-08-15 01:31:14 +0300

    create migration.

---
+++ src/Services/Main/Main.Migrator/Migrations/20260814223019_CatalogueCandidateIdUpdate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Main.Migrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class CatalogueCandidateIdUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "catalogue_candidate_id",
+                schema: "catalogue_enrichment",
+                table: "supplier_products",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "catalogue_enrichment",
+                table: "catalogue_candidates",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "catalogue_candidate_id",
+                schema: "catalogue_enrichment",
+                table: "supplier_products",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                schema: "catalogue_enrichment",
+                table: "catalogue_candidates",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
