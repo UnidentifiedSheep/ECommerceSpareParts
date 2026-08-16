56a87af4fc8d338313cb385852d6c95a0239e5b1
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-08-16 14:26:50 +0300

    remove legacy endpints, handlers.

--- src/Services/Search/Search.Application/Interfaces/Product/IProductRepository.cs
+++ src/Services/Search/Search.Application/Interfaces/Product/IProductRepository.cs
@@ -1,6 +1,4 @@
-using Abstractions.Models;
 using Search.Application.Models.CatalogueSearch;
-using Search.Enums;
 
 namespace Search.Application.Interfaces.Product;
 
@@ -10,35 +8,4 @@ public interface IProductRepository
     Task<SearchResult<Entities.Product>> Search(
         CatalogueSearchCriteria criteria,
         CancellationToken cancellationToken = default);
-
-    Task<IReadOnlyCollection<Entities.Product>> Search(
-        string query,
-        int? producerId = null,
-        Pagination? pagination = null,
-        string[]? sortBy = null,
-        RangeModel<decimal>? lengthM = null,
-        RangeModel<decimal>? widthM = null,
-        RangeModel<decimal>? heightM = null,
-        CancellationToken token = default);
-
-    Task<IReadOnlyCollection<Entities.Product>> SearchBySku(
-        string? sku,
-        int? producerId,
-        SkuSearchMode searchMode,
-        Pagination? pagination = null,
-        string[]? sortBy = null,
-        CancellationToken token = default);
-
-    Task<IReadOnlyCollection<Entities.Product>> GetByWeightKgRange(
-        RangeModel<decimal>? weightKg = null,
-        Pagination? pagination = null,
-        string[]? sortBy = null,
-        CancellationToken token = default);
-
-    Task<IReadOnlyCollection<Entities.Product>> GetByVolumeM3Range(
-        RangeModel<decimal>? volumeM3 = null,
-        Pagination? pagination = null,
-        string[]? sortBy = null,
-        CancellationToken token = default);
-
 }
