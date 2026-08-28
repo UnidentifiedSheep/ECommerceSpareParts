a846d7231ecaf3cf71fc078d24a52675cd0fe10b
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-08-28 23:34:13 +0300

    fix handlers

--- src/Services/Main/Main.Application/DomainEventHandlers/Product/ProductWeightUpdatedHandler.cs
+++ src/Services/Main/Main.Application/DomainEventHandlers/Product/ProductWeightUpdatedHandler.cs
@@ -10,20 +10,15 @@ using ZiggyCreatures.Caching.Fusion;
 namespace Main.Application.DomainEventHandlers.Product;
 
 public class ProductWeightUpdatedHandler(
-    IFusionCache cache,
-    ICachePolicy<GetProductWeightQuery> cachePolicy,
     IIntegrationEventScope integrationEventScope) : BatchableDomainEventHandler<ProductWeightUpdatedDomainEvent>
 {
-    public override async Task Handle(Batch<ProductWeightUpdatedDomainEvent> notification, CancellationToken cancellationToken)
+    public override Task Handle(
+        Batch<ProductWeightUpdatedDomainEvent> notification, 
+        CancellationToken cancellationToken)
     {
-        var keys = new List<string>(notification.Items.Count);
         foreach (var i in notification.Items)
-        {
             integrationEventScope.Add(new ProductUpdatedEvent { Id = i.ProductId });
-            keys.Add(cachePolicy.GetCacheKey(new GetProductWeightQuery(i.ProductId)));
-        }
-
-        foreach (var key in keys)
-            await cache.RemoveAsync(key, token: cancellationToken);
+        
+        return Task.CompletedTask;
     }
 }
