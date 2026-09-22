5ee94eb60e2ec5507527f601872e9a5e463a31d5
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-09-23 00:22:01 +0300

    create inapp entity and configuration

--- src/Infrastructure/Notifications/Notification.Persistence/ModelBuilderExtensions.cs
+++ src/Infrastructure/Notifications/Notification.Persistence/ModelBuilderExtensions.cs
@@ -6,5 +6,6 @@ public static class ModelBuilderExtensions
 {
 	public static ModelBuilder AddNotifications(this ModelBuilder builder)
 		=> builder.ApplyConfiguration(new NotificationConfiguration())
-			.ApplyConfiguration(new NotificationDeliveryConfiguration());
+			.ApplyConfiguration(new NotificationDeliveryConfiguration())
+			.ApplyConfiguration(new InAppNotificationConfiguration());
 }
