efbbe9d709f8f62670b0cb9ffb4836b31fd44c84
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-09-22 11:11:01 +0300

    create table configuration

---
+++ src/Infrastructure/Notifications/Notification.Persistence/ModelBuilderExtensions.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Notification.Persistence;
+
+public static class ModelBuilderExtensions
+{
+	public static ModelBuilder AddNotifications(this ModelBuilder builder)
+		=> builder.ApplyConfiguration(new NotificationConfiguration())
+			.ApplyConfiguration(new NotificationDeliveryConfiguration());
+}
