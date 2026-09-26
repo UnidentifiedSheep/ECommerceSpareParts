8989f00d025b758c2596b5b277529be471caef14
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-09-26 15:22:42 +0300

    add notification preference dto, handler etc

--- src/Infrastructure/Notifications/Notification.Core/Recipients/Recipients.cs
+++ src/Infrastructure/Notifications/Notification.Core/Recipients/Recipients.cs
@@ -2,7 +2,7 @@ namespace Notification.Core.Recipients;
 
 public static class Recipients
 {
-	public static IReadOnlyDictionary<string, Type> All = new Dictionary<string, Type>
+	public static IReadOnlyDictionary<string, Type> All { get; } = new Dictionary<string, Type>
 	{
 		[InAppRecipient.ChannelName] = typeof(InAppRecipient),
 		[EmailRecipient.ChannelName] = typeof(EmailRecipient)
---
+++ src/Services/Main/Main.Application/Handlers/Users/NotificationPreferences/GetNotificationPreferencesHandler.cs
@@ -0,0 +1,45 @@
+using Application.Common.Extensions;
+using Application.Common.Interfaces.Cqrs;
+using Application.Common.Interfaces.Projections;
+using Application.Common.Interfaces.Repositories;
+using Locan.Core.Interfaces.Localizers;
+using Main.Application.Dtos.Users;
+using Main.Entities;
+using Main.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using Notification.Core.Recipients;
+
+namespace Main.Application.Handlers.Users.NotificationPreferences;
+
+public record GetNotificationPreferencesQuery(Guid UserId) : IQuery<GetNotificationPreferencesResult>;
+
+public record GetNotificationPreferencesResult(
+	IReadOnlyList<UserNotificationPreferenceDto> Preferences);
+
+public class GetNotificationPreferencesHandler(
+	IReadRepository<UserNotificationPreference, UserNotificationPreferenceKey> repository,
+	IProjectionProvider<UserNotificationPreference, UserNotificationPreferenceDto> projection,
+	IContextualLocalizer localizer
+	) : IQueryHandler<GetNotificationPreferencesQuery, GetNotificationPreferencesResult>
+{
+	public async Task<GetNotificationPreferencesResult> Handle(
+		GetNotificationPreferencesQuery request,
+		CancellationToken cancellationToken)
+	{
+		var userPreferences = await repository.Query
+			.Where(x => x.UserId == request.UserId)
+			.Project(projection)
+			.ToListAsync(cancellationToken);
+
+		if (userPreferences.All(x => x.ChannelName != InAppRecipient.ChannelName))
+			userPreferences.Add(new UserNotificationPreferenceDto
+			{
+				ChannelName = InAppRecipient.ChannelName,
+				Enabled = true,
+				LocalizableChannelName = localizer.Get(InAppChannelMessage.Instance),
+				UserId = request.UserId
+			});
+
+		return new GetNotificationPreferencesResult(userPreferences);
+	}
+}
